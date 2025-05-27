2f3b988fa69c84687b555b9ba8647ffa89d29af2
Meni901 <meniramos3046@gmail.com>
2025-05-27 09:30:38 -0300

    Delete sistema-rpg-main directory

--- sistema-rpg-main/frontend/js/dados.js
+++
@@ -1,7 +0,0 @@
-function rolarDado(lados) {
-  const resultado = Math.floor(Math.random() * lados) + 1;
-  const display = document.getElementById("resultado-dado");
-  if (display) {
-    display.textContent = `Resultado: ${resultado}`;
-  }
-}
